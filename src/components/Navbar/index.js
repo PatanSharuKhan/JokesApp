8ddda23b4c20491753a0d9a630b6284fe22e239b
patansharukhan <patansharu654321@gmail.com>
2023-06-13 02:39:22 +0530

    Added Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+const Navbar = () => (
+  <div className="container d-flex p-2 justify-content-between align-items-center">
+    <h1>Navbar</h1>
+    <button type="button" className="btn btn-primary">
+      Logout
+    </button>
+  </div>
+)
+
+export default Navbar
