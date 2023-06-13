ca951b94b885b6b952c40c809c8b5555e93713f7
patansharukhan <patansharu654321@gmail.com>
2023-06-13 10:26:54 +0530

    added logout btn and features

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,33 @@
-const Navbar = () => (
-  <div className="container d-flex p-2 justify-content-between align-items-center">
-    <h1>Navbar</h1>
-    <button type="button" className="btn btn-primary">
-      Logout
-    </button>
-  </div>
+import { useNavigate } from "react-router-dom"
+import "./index.css"
+
+// --------------------[ components ]-----------------------------
+
+const Button = ({ removePermission }) => (
+  <button type="button" className="btn btn-primary" onClick={removePermission}>
+    Logout
+  </button>
 )
+
+// --------------------[ main ]-----------------------------
+
+const Navbar = () => {
+  const navigate = useNavigate()
+
+  const removePermission = () => {
+    localStorage.removeItem("userDetails")
+    navigate("/login")
+  }
+
+  return (
+    <>
+      <div className="container d-flex p-2 justify-content-between align-items-center">
+        <h1>Navbar</h1>
+        <Button removePermission={removePermission} />
+      </div>
+      <hr className="container" />
+    </>
+  )
+}
 
 export default Navbar
